41c4d6b1b91a624198c05a8284a3754e0701aac8
Lejun Chen <you@example.com>
2021-08-12 07:11:07 +0200

    menu grid to flex

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -7,14 +7,16 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function Menu({ menuItem }) {
   return (
     <MenuItemStyled>
-      {menuItem.map((item) => {
-        return (
-          <div className="grid-item" key={item.id}>
-            <div className="portfolio-content">
-              <Link to={item.internalLink}>
-                <div className="portfolio-image">
-                  <img src={item.image} alt="" />
-                  {/* <ul>
+      <div className="portfolio-container">
+        <div className="items">
+          {menuItem.map((item) => {
+            return (
+              <div className="grid-item" key={item.id}>
+                <div className="portfolio-content">
+                  <Link to={item.internalLink}>
+                    <div className="portfolio-image">
+                      <img src={item.image} alt="" />
+                      {/* <ul>
                                     <li>
                                         <a href={item.link1}>
                                             <GitHub />
@@ -26,24 +28,26 @@ function Menu({ menuItem }) {
                                         </a>
                                     </li>
                                 </ul> */}
-                  <div className="container">
-                    <div className="content">
-                      <h6 className="title">{item.title}</h6>
-                      <p>{item.text}</p>
+                      <div className="content-container">
+                        <div className="content">
+                          <h6 className="title">{item.title}</h6>
+                          <p>{item.text}</p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          </div>
-        );
-      })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </MenuItemStyled>
   );
 }
 
 const MenuItemStyled = styled.div`
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 1rem;
   @media screen and (max-width: 1600px) {
@@ -55,21 +59,55 @@ const MenuItemStyled = styled.div`
   @media screen and (max-width: 670px) {
     grid-template-columns: repeat(1, 1fr);
     grid-gap: 1rem;
+  } */
+  /* display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 0, auto;
+  .portfolio-container {
+ 
+  } */
+
+  .portfolio-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0, auto;
+  }
+  .items {
+    width: fit-content;
+    display: inline-flex !important;
+    width: auto;
+    flex-direction: row;
+    margin: 0, auto;
+    /* justify-content: center; */
+    /* align-items: center; */
+    /* max-width: 1200px; */
+    flex-wrap: wrap;
+    vertical-align: top;
+    box-sizing: border-box;
+    width: 100%;
   }
   .grid-item {
     .portfolio-content {
+      flex-grow: 1;
       display: block;
       position: relative;
       overflow: hidden;
       border-radius: 7px;
+      width: 370px;
+      height: 220px;
+      margin: 15px;
       img {
-        width: 100%;
-        /* height: 40vh; */
-        height: 100%;
+        /* width: 100%;
+        height: 100%; */
+        width: 370px;
+        height: 220px;
         object-fit: cover;
         transition: all 0.4s ease-in-out;
+        margin: 0;
       }
-      .container {
+      .content-container {
         width: 100%;
         height: 100%;
         position: absolute;
@@ -78,6 +116,7 @@ const MenuItemStyled = styled.div`
         display: flex;
         flex-direction: column;
         align-items: center;
+
         margin: auto 0;
       }
       .content {
@@ -85,61 +124,33 @@ const MenuItemStyled = styled.div`
         width: 100%;
         transition: all 0.5s ease-in-out;
         opacity: 1;
-        /* padding: 0.5rem 0.5rem; */
         /* margin: auto 0; */
         text-align: justify;
-        transform: translateY(85%);
+        transform: translateY(86%);
         height: 100%;
+        justify-content: center;
       }
       a {
+        display: block;
         text-decoration: none;
+        width: 100%;
+        height: 100%;
+        margin: 0;
       }
       h6,
       p {
         font-size: 0.9rem;
         text-align: left;
-        /* padding-bottom: 0; */
-        width: 100%;
+        margin: 10px;
         color: var(--primary-color);
         display: block;
-        position: relative;
         /* text-align: justify; */
       }
       .title {
         padding: 0;
         margin-top: 1%;
         font-size: inherit;
-        /* text-align: reverted; */
       }
-      /* ul{
-                transform: translateY(-600px);
-                transition: all .4s ease-in-out;
-                position: absolute;
-                left: 50%;
-                top: 40%;
-                opacity: 0;
-                li{
-                        background-color: var(--border-color);
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        padding: 1rem;
-                        border-radius: 50%;
-                        width: 3rem;
-                        height: 3rem;
-                        margin: 0 .5rem;
-                        transition: all .4s ease-in-out;
-                        &:hover{
-                            background-color: var(--primary-color);
-                        }
-                        a{
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
-                            transition: all .4s ease-in-out;
-                        }
-                    }
-            } */
 
       .portfolio-image {
         transition: all 0.4s ease-in-out;
@@ -151,9 +162,7 @@ const MenuItemStyled = styled.div`
           width: 100%;
           background-color: var(--background-dark-color);
           opacity: 0.8;
-          height: 100%;
-          width: 100%;
-          transform: translateY(85%);
+          transform: translateY(86%);
           transition: 0.5s;
         }
       }
@@ -162,12 +171,7 @@ const MenuItemStyled = styled.div`
         transition: all 0.4s ease-in-out;
         .content {
           transform: translateY(0);
-          /* transform: translate(-50%, -50%); */
-          /* display: flex; */
-          /* align-items: center;
-          justify-content: center; */
           transition: all 0.4s ease-in-out;
-          opacity: 1;
         }
         &::before {
           height: 100%;
@@ -175,36 +179,6 @@ const MenuItemStyled = styled.div`
           transform: translateY(0);
           opacity: 0.9;
         }
-        /* ul {
-          transform: translateY(0);
-          transform: translate(-50%, -50%);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          transition: all 0.4s ease-in-out;
-          opacity: 1;
-          li {
-            transition: all 0.4s ease-in-out;
-            &:hover {
-              background-color: var(--primary-color);
-            }
-            a {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              transition: all 0.4s ease-in-out;
-            }
-          }
-
-          li:hover {
-            svg {
-              color: var(--white-color);
-            }
-          }
-          svg {
-            font-size: 2rem;
-          }
-        } */
       }
     }
   }
